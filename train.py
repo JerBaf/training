82ccff25b87ee6a1ac58e3d2fdec72912934a62a
JerBaf <jeremybaffou@yahoo.fr>
2025-04-30 14:05:46 +0200

    finish class architecture and code

--- train.py
+++ train.py
@@ -1,15 +1,12 @@
 # Packages
-from dataset import get_datasets, TemporalDataLoader
 from functools import partial
-from models.vae_temporal import categorical_loss_fn, VAE
 import numpy as np
 import os
 import random
-from sklearn.metrics import classification_report, mean_squared_error, roc_auc_score
+from sklearn.metrics import classification_report, mean_squared_error, mean_absolute_error, roc_auc_score
 import torch
 from torch.nn import Softmax
-from torch.utils.data import Dataset
-from torch_geometric.loader import DataLoader
+from torch.utils.data import Dataset, WeightedRandomSampler
 import wandb
 from wandb.sdk.wandb_config import Config
 
@@ -24,7 +21,7 @@ class Metric_Calculator():
         """ Compute the Pytorch loss for backpropagation"""
         return torch.Tensor()
 
-    def compute_loss_and_metrics(self,y_true,y_pred) -> tuple:
+    def compute_loss_and_metrics(self,batch,y_pred) -> tuple:
         """ Compute loss and task-associated metrics. """
         loss, metric_dict = None, None
         return loss, metric_dict
@@ -34,11 +31,11 @@ class Metric_Calculator():
         """ Compute classification related metrics. """
         # Labels and predictions formatting
         s_func = Softmax(dim=-1)
-        labels = np.concatenate([np.copy(np.array(l.cpu())).argmax(axis=1) for l in labels],axis=0)
-        probs = np.concatenate([np.copy(np.array(s_func(p.detach().cpu()))) for p in pred],axis=0)
-        pred = np.concatenate([np.copy(np.array(p.detach().cpu())).argmax(axis=1) for p in pred],axis=0)
+        labels = np.concatenate([np.copy(np.array(l.cpu())).argmax(axis=1) for l in y_true],axis=0)
+        probs = np.concatenate([np.copy(np.array(s_func(p.detach().cpu()))) for p in y_pred],axis=0)
+        pred = np.concatenate([np.copy(np.array(p.detach().cpu())).argmax(axis=1) for p in y_pred],axis=0)
         # Retrieve label list
-        label_list = [str(i) for i in range(config.num_classes)] 
+        label_list = [str(i) for i in range(self.config.num_classes)] 
         # Metrics computation
         metric_dict = dict()
         metrics = classification_report(labels,pred,labels=label_list,output_dict=True)
@@ -52,40 +49,38 @@ class Metric_Calculator():
         weighted_f1 = metrics['weighted avg']['f1-score']
         macro_f1 = metrics['macro avg']['f1-score']
         # Metrics Registration
-        metric_dict[f'{prefix}_loss'] = loss
-        cross_val_dict[f'{prefix}_loss'].append(loss)
-        metric_dict[f'{prefix}_auc'] = auc
-        cross_val_dict[f'{prefix}_auc'].append(auc)
-        metric_dict[f'{prefix}_weighted_f1'] = weighted_f1
-        cross_val_dict[f'{prefix}_weighted_f1'].append(weighted_f1)
-        metric_dict[f'{prefix}_macro_f1'] = macro_f1
-        cross_val_dict[f'{prefix}_macro_f1'].append(macro_f1)
+        metric_dict['auc'] = auc
+        metric_dict['weighted_f1'] = weighted_f1
+        metric_dict['macro_f1'] = macro_f1
         # Recall and precision registration
         for label_id in label_list:
             for metric_type in ['recall','precision']:
                 metric_value = metrics[label_id][metric_type]
-                metric_dict[f'{prefix}_{metric_type}_{label_id}'] = metric_value
-                cross_val_dict[f'{prefix}_{metric_type}_{label_id}'].append(metric_value)
-        wandb.log(metric_dict)
-        # Update confusion matrix 
-        if macro_f1 >= np.max(cross_val_dict[f'{prefix}_macro_f1']):
-            cross_val_dict[f'{prefix}_best_model_labels'] = labels
-            cross_val_dict[f'{prefix}_best_model_preds'] = pred
+                metric_dict[f'{metric_type}_{label_id}'] = metric_value
+        return metric_dict
 
     @classmethod
     def compute_regression_metric(self,y_true,y_pred) -> dict:
         """ Compute regression related metrics. """
-
-
-    
+        # Labels and predictions formatting
+        labels = np.concatenate([np.copy(np.array(l.cpu())) for l in y_true],axis=0)
+        preds = np.concatenate([np.copy(np.array(p.detach().cpu())) for p in y_pred],axis=0)
+        # Metric computation
+        metric_dict = dict()
+        mse = mean_squared_error(labels,preds)
+        mae = mean_absolute_error(labels,preds)
+        # Metrics Registration
+        metric_dict['mse'] = mse
+        metric_dict['mae'] = mae
+        return metric_dict
 
 class Logger():
     """ Class to log the results through wandb. """
 
-    def __init__(self,config:Config,metric_list:list):
+    def __init__(self,config:Config):
         self.config = config
         self.cross_val_dict = self.create_metric_dict()
-        self.metric_list = metric_list
+        self.metric_list = config.metric_list
 
     def get_metric_list(self) -> list:
         """ Get the list of metrics. """
@@ -111,11 +106,29 @@ class Logger():
 class Trainer():
     """ Class to perform cross-validation training. """
 
-    def __init__(self,config:Config,fold_id:int,metric_list:list):
+    def __init__(self,config:Config,fold_id:int):
         self.config = config
         self.device = torch.device(config.device)
         self.fold_id = fold_id
-        self.logger = Logger(config,metric_list)
+        self.metric_calculator = Metric_Calculator(config)
+        self.logger = Logger(config)
+
+    def create_balanced_sampler(self,dataset) -> WeightedRandomSampler:
+        """ Create a balanced sampler for training based on class distribution."""
+        labels = dict([(i,0) for i in range(self.config.num_classes)])
+        # Compute label distribution
+        for idx in range(len(dataset)):
+            data = dataset.get(idx)
+            label = torch.argmax(data[-1].y).item()
+            labels[label] += 1
+        # Compute weights
+        weights = dict([(k,len(dataset)/v) for k,v in labels.items()])
+        weight_list = []
+        for idx in range(len(dataset)):
+            data = dataset.get(idx)
+            label = torch.argmax(data[-1].y).item()
+            weight_list.append(weights[label])
+        return WeightedRandomSampler(weight_list,len(dataset))
 
     def create_model(self) -> torch.nn.Module:
         """ Create an instance of the model defined in the config file. """
@@ -163,7 +176,7 @@ class Trainer():
             for batch in dataloader:
                 # Forward Pass
                 preds = model.forward(batch)
-                loss, metric_values = compute_loss_and_metrics
+                loss, metric_values = self.metric_calculator.compute_loss_and_metrics(batch,preds)
                 # Update using the gradients
                 if split == 'train':
                     optimizer.zero_grad()
@@ -179,7 +192,6 @@ class Trainer():
         if self.config.save_path != '' and cross_val_dict[f'val_total_loss'][-1] <= np.min(cross_val_dict[f'val_total_loss']):
             torch.save(model,os.path.join(config.save_path,f'model_fold_{fold_id}_best_loss.pth'))
 
-
     def set_seed(self) -> torch.Generator:
         """ Setup reproducibility for a set of libraries. """
         seed = self.config.seed
@@ -189,126 +201,6 @@ class Trainer():
         generator = torch.Generator()
         generator.manual_seed(seed)
         return generator
-
-
-# Train Helpers
-
-def get_model(input_dim:int,config:Config,device):
-    """ Retrieve the model based on the given config. """
-    if config.model_type == 'vae':
-        model = VAE(input_dim,config)
-        for roland_layer in model.patient_encoder.backbone.rnn_layers:
-            roland_layer.to(device)
-    else:
-        raise ValueError(f'Model type {config.model_type} is not implemented.')
-    model.to(device)
-    return model
-
-def create_target_dict(batch,config:Config) -> dict: 
-    """ Retrieve target for all lesions/patient across all time points. """
-    target_dict = {'lesion':dict(),'patient':dict()}
-    device = torch.device('mps')
-    for t in range(config.time_steps):
-        # Lesion
-        volume_tensor = batch[t].x[:,1:-1] #batch[t].x[:,9:-1]
-        target_dict['lesion'][t] = volume_tensor.to(device)
-        # Patient
-        volume_tensor = batch[t+1].x[batch[t+1].update_index,-1]
-        target_dict['patient'][t] = volume_tensor.to(device)
-    return target_dict
-
-def train_one_epoch(optimizer,model,train_loader,val_loader,
-                    fold_id:int,cross_val_dict:dict,
-                    config:Config,temperature:float):
-    """ Train function for one whole epochs. 
-
-        :param optimizer: Pytorch optimizer to minimze the loss function.
-        :param model: GNN model to make the predictions.
-        :param train_loader: Graph DataLoader in batch format for training data.
-        :param val_loader: Graph DataLoader in batch format for validation data.
-        :param loss_fn: Pytorch loss function to be minimized.
-        :param device: Device on which the computation is made.
-        :param fold_id: Index of the fold kept apart as validation.
-        :param cross_val_dict: Dictionnary to store evolution of values across folds.
-        :param target: Choose between relapse or histological grade for the target.
-
-    """
-    dataloader_dict = {'train':train_loader,'val':val_loader}
-    # Model training
-    for split, dataloader in dataloader_dict.items():
-        # Set training mode
-        if split == 'train':
-            model.train()
-        else:
-            model.eval()
-        # Batch loop
-        for batch in dataloader:
-            # Retrieve volumes
-            target_dict = create_target_dict(batch,config)
-            # Forward Pass
-            lesion_register, patient_register = model.forward(batch,temperature)
-            loss, loss_dict = categorical_loss_fn(lesion_register,patient_register,target_dict,config.time_steps,config.beta)
-            # Update using the gradients
-            if split == 'train':
-                optimizer.zero_grad()
-                loss.backward()  
-                optimizer.step()
-        # Register metrics
-        update_metrics(cross_val_dict,loss_dict,split,temperature)
-    # Save if save path provided
-    #if config.save_path != '' and cross_val_dict[f'val_total_loss'][-1] <= np.min(cross_val_dict[f'val_total_loss']):
-    torch.save(model,os.path.join(config.save_path,f'model_fold_{fold_id}_best_loss.pth'))
-
-def train(train_loader:DataLoader,val_loader:DataLoader,input_dim:int,fold_id:int,cross_val_dict:dict,config:Config):
-    """ Full end-to-end training of a model. 
-    
-        :param train_loader: Graph DataLoader in batch format for training data.
-        :param val_loader: Graph DataLoader in batch format for validation data.
-        :param input_dim: Input dimension of node features.
-        :param fold_id: Index of the fold kept apart as validation.
-        :param cross_val_dict: Dictionnary to store evolution of values across folds.
-        :param config: Wandb config object with all training parameters.
-        
-    """
-    # Model initialization
-    device = torch.device('mps')
-    model = get_model(input_dim,config,device)
-    # Optimization modules
-    optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
-    # Model training
-    for epoch in range(config.epochs):
-        temp = np.maximum(config.temp_0 * np.exp(-config.anneal_rate * epoch), config.temp_min)
-        train_one_epoch(optimizer,model,train_loader,val_loader,
-                        fold_id,cross_val_dict,config,temp)
-    # Release memory
-    del model
-
-def train_one_fold(fold_id:int,config:Config,cross_val_dict:dict):
-    """ Full pipeline for model training, leaving one fold for validation.
-    
-        :param fold_id: Index of the fold kept apart as validation.
-        :param config: Wandb config object with all training parameters.
-        :param cross_val_dict: Dictionnary to store evolution of values across folds.
-    
-    """
-    # Reproducibility
-    torch.manual_seed(config.seed)
-    random.seed(config.seed)
-    np.random.seed(config.seed)
-    generator = torch.Generator()
-    generator.manual_seed(config.seed)
-    # Datasets
-    train_dataset_path = os.path.join(config.data_paths[str(fold_id)],'train')
-    val_dataset_path = os.path.join(config.data_paths[str(fold_id)],'val')
-    training_dataset, validation_dataset = get_datasets(train_dataset_path,val_dataset_path,config)
-    # Retrieve key parameters
-    example_data_point = training_dataset.get(0)
-    input_dim = example_data_point[0].x.shape[1]
-    # DataLoaders
-    training_loader = TemporalDataLoader(training_dataset,batch_size=config.batch_size,generator=generator,follow_batch=['edge_index'],shuffle=True)
-    validation_loader = TemporalDataLoader(validation_dataset,batch_size=config.batch_size,generator=generator,follow_batch=['edge_index'],shuffle=True)
-    # Training
-    train(training_loader,validation_loader,input_dim,fold_id,cross_val_dict,config)
 
 # Grid Search Helpers
 
@@ -341,7 +233,7 @@ def create_sweep_config(parameters_dict:dict,method:str='grid') -> dict:
     sweep_config['parameters'] = parameters_dict
     return sweep_config
 
-def gnn_pipeline(parameters_dict:dict,project_name:str,method:str='grid'):
+def training_pipeline(parameters_dict:dict,project_name:str,method:str='grid'):
     """ Hyper parameter search with cross-validation for a GNN. """
     # Process to cross validation
     std_dict = dict()
@@ -350,13 +242,13 @@ def gnn_pipeline(parameters_dict:dict,project_name:str,method:str='grid'):
         sweep_config = create_sweep_config(parameters_dict,method=method)
         sweep_id = wandb.sweep(sweep_config, project=project_name)
         # Launch training
-        wandb.agent(sweep_id,partial(gnn_single_fold,fold_id=fold_id,std_dict=std_dict))
+        wandb.agent(sweep_id,partial(training_single_fold,fold_id=fold_id,std_dict=std_dict))
     # Log standard deviations
     sweep_config = create_sweep_config(parameters_dict,method=method)
     sweep_id = wandb.sweep(sweep_config, project=project_name)
     wandb.agent(sweep_id,partial(std_logging,std_dict=std_dict))
 
-def gnn_single_fold(config:Config=None,fold_id:int=0,std_dict:dict=dict()):
+def training_single_fold(config:Config=None,fold_id:int=0,std_dict:dict=dict()):
     """ Training and metric registration for a single fold
         
         :param config: Wandb config for the hyperparameter search.
@@ -367,10 +259,11 @@ def gnn_single_fold(config:Config=None,fold_id:int=0,std_dict:dict=dict()):
     with wandb.init(config=config):
         config = wandb.config
         # Perform training
-        cross_val_dict = create_metric_dict(config)
-        train_one_fold(fold_id,config,cross_val_dict)
+        trainer = Trainer(config,fold_id)
+        trainer.train()
         # Assign target specific variables
-        metric_list = get_metric_list(config)
+        metric_list = trainer.logger.metric_list
+        cross_val_dict = trainer.logger.cross_val_dict
         # Log global metrics
         global_metric_dict = dict()
         for metric_name, metric_values in cross_val_dict.items():
