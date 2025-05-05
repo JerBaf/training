14a6fe6dc44f3121db575c8629e25fff08aeaa61
JerBaf <jeremybaffou@yahoo.fr>
2025-05-05 15:08:20 +0200

    update metrics computation and confusion matrix logging

--- train.py
+++ train.py
@@ -17,17 +17,25 @@ class Metric_Calculator():
     def __init__(self,config:Config):
         self.config = config
 
-    def compute_loss(self,y_true,y_pred) -> torch.Tensor:
-        """ Compute the Pytorch loss for backpropagation"""
-        return torch.Tensor()
+    def compute_loss(self,y_true:torch.Tensor,y_pred:torch.Tensor) -> torch.Tensor:
+        """ Compute the Pytorch loss for backpropagation. """
+        loss = None
+        return loss
 
-    def compute_loss_and_metrics(self,batch,y_pred) -> tuple:
+    def compute_loss_and_metrics(self,pred_dict:dict,split:str) -> tuple:
         """ Compute loss and task-associated metrics. """
-        loss, metric_dict = None, None
-        return loss, metric_dict
+        # Compute task-associated metrics
+        y_true, y_pred = pred_dict[split]['y_true'], pred_dict[split]['y_pred']
+        if self.config.task_type == 'classification':
+            metric_dict = self.compute_classification_metrics(y_true,y_pred)
+        else:
+            metric_dict = self.compute_regression_metric(y_true,y_pred)
+        # Compute loss
+        loss = self.compute_loss(y_true,y_pred)
+        metric_dict['loss'] = loss
+        return metric_dict
 
-    @classmethod
-    def compute_classification_metrics(self,y_true,y_pred) -> dict:
+    def compute_classification_metrics(self,y_true:torch.Tensor,y_pred:torch.Tensor) -> dict:
         """ Compute classification related metrics. """
         # Labels and predictions formatting
         s_func = Softmax(dim=-1)
@@ -46,12 +54,12 @@ class Metric_Calculator():
         else:
             auc = roc_auc_score(labels,probs,multi_class='ovo',labels=[int(l) for l in label_list])    
         # Retrieve averages
-        weighted_f1 = metrics['weighted avg']['f1-score']
         macro_f1 = metrics['macro avg']['f1-score']
+        weighted_f1 = metrics['weighted avg']['f1-score']
         # Metrics Registration
         metric_dict['auc'] = auc
-        metric_dict['weighted_f1'] = weighted_f1
         metric_dict['macro_f1'] = macro_f1
+        metric_dict['weighted_f1'] = weighted_f1
         # Recall and precision registration
         for label_id in label_list:
             for metric_type in ['recall','precision']:
@@ -59,8 +67,7 @@ class Metric_Calculator():
                 metric_dict[f'{metric_type}_{label_id}'] = metric_value
         return metric_dict
 
-    @classmethod
-    def compute_regression_metric(self,y_true,y_pred) -> dict:
+    def compute_regression_metric(self,y_true:torch.Tensor,y_pred:torch.Tensor) -> dict:
         """ Compute regression related metrics. """
         # Labels and predictions formatting
         labels = np.concatenate([np.copy(np.array(l.cpu())) for l in y_true],axis=0)
@@ -79,28 +86,37 @@ class Logger():
 
     def __init__(self,config:Config):
         self.config = config
-        self.cross_val_dict = self.create_metric_dict()
+        self.fold_metric_dict = self.create_metric_dict()
         self.metric_list = config.metric_list
 
+    def create_metric_dict(self) -> dict:
+        """ Create a dictionnary to register metrics. """
+        metric_list = self.get_metric_list()
+        return dict(zip(metric_list,[list() for _ in range(len(metric_list))]))
+    
     def get_metric_list(self) -> list:
         """ Get the list of metrics. """
         full_metric_list = []
         full_metric_list.extend(['train_'+m for m in self.metric_list])
         full_metric_list.extend(['val_'+m for m in self.metric_list])
         return full_metric_list
-    
-    def create_metric_dict(self) -> dict:
-        """ Create a dictionnary to register metrics. """
-        metric_list = self.get_metric_list()
-        return dict(zip(metric_list,[list() for _ in range(len(metric_list))]))
-    
-    def update_metrics(self,metric_values:dict,prefix:str):
+     
+    def log_confusion_matrix(self,pred_dict:dict):
+        """ Register the confusion matrices on wandb. """
+        if self.config.task_type == 'classification':
+            for split in ['train','val']:
+                y_true, y_pred = pred_dict[split]['y_true'], pred_dict[split]['y_pred']
+                wandb.log({f"{split}_conf_mat" : wandb.plot.confusion_matrix(probs=None,
+                                                    y_true=y_true, preds=y_pred,
+                                                    class_names=self.config.class_names)})
+
+    def update_metrics(self,metric_values:dict,prefix:str,y_true:list=None,y_pred:list=None):
         """ Update and register the metrics on wandb. """
         metric_dict = dict()
         # Metrics Registration
         for metric_name, metric_value in metric_values.items():
             metric_dict[f'{prefix}_{metric_name}'] = metric_value
-            self.cross_val_dict[f'{prefix}_{metric_name}'].append(metric_value)
+            self.fold_metric_dict[f'{prefix}_{metric_name}'].append(metric_value)
         wandb.log(metric_dict)
 
 class Trainer():
@@ -149,6 +165,16 @@ class Trainer():
         input_dim = example_data_point.x.shape[1]
         return input_dim
 
+    def is_best_model(self) -> bool:
+        """ Boolean indicator if the last epoch resulted in the best trained model so far. """
+        fold_metric_dict = self.logger.fold_metric_dict
+        target_metric = f'val_{self.config.target_metric}'
+        if self.config.save_mode == 'min':
+            metric_condition = fold_metric_dict[target_metric][-1] <= np.min(fold_metric_dict[target_metric])
+        else:
+            metric_condition = fold_metric_dict[target_metric][-1] >= np.max(fold_metric_dict[target_metric])
+        return metric_condition
+        
     def train(self):
         """ Perform full training for one fold in cross-validation. """
         # Initialization
@@ -165,6 +191,8 @@ class Trainer():
     def train_one_epoch(self,epoch,model,optimizer,train_loader,val_loader):
         """ Train the model for a single epoch. """
         dataloader_dict = {'train':train_loader,'val':val_loader}
+        pred_dict = {'train':{'y_true':torch.Tensor(),'y_pred':torch.Tensor()},
+                     'val':{'y_true':torch.Tensor(),'y_pred':torch.Tensor()}}
         # Model training
         for split, dataloader in dataloader_dict.items():
             # Set training mode
@@ -175,23 +203,28 @@ class Trainer():
             # Batch loop
             for batch in dataloader:
                 # Forward Pass
-                preds = model.forward(batch)
-                loss, metric_values = self.metric_calculator.compute_loss_and_metrics(batch,preds)
+                y_true, y_pred = batch.y, model.forward(batch)
+                loss = self.metric_calculator.compute_loss(y_true,y_pred)
                 # Update using the gradients
                 if split == 'train':
                     optimizer.zero_grad()
                     loss.backward()  
                     optimizer.step()
+                # Update running list of targets/preds
+                pred_dict[split]['y_true'] = torch.cat((pred_dict[split]['y_true'],y_true),dim=0)
+                pred_dict[split]['y_pred'] = torch.cat((pred_dict[split]['y_pred'],y_pred),dim=0)
             # Register metrics
+            metric_values = self.metric_calculator.compute_loss_and_metrics(pred_dict,split)
             self.logger.update_metrics(metric_values,split)
         # Save if save path provided
-        self.save_model(model)
+        self.save_model(model,pred_dict)
         
-    def save_model(self,model:torch.nn.Module):
-        """ Save the model if the config allows it. """
-        if self.config.save_path != '' and cross_val_dict[f'val_total_loss'][-1] <= np.min(cross_val_dict[f'val_total_loss']):
-            torch.save(model,os.path.join(config.save_path,f'model_fold_{fold_id}_best_loss.pth'))
-
+    def save_model(self,model:torch.nn.Module,pred_dict:dict):
+        """ If the config allows it, save the model and register the confusion matrices. """
+        if self.config.save_path != '' and self.is_best_model():
+            torch.save(model,os.path.join(self.config.save_path,f'best_model_fold_{self.fold_id}.pth'))
+            self.logger.log_confusion_matrix(pred_dict)
+        
     def set_seed(self) -> torch.Generator:
         """ Setup reproducibility for a set of libraries. """
         seed = self.config.seed
@@ -203,13 +236,6 @@ class Trainer():
         return generator
 
 # Grid Search Helpers
-
-def std_logging(config:Config=None,std_dict:dict=dict()):
-    """ Register the std for all metrics. """
-    # Log standard deviations
-    with wandb.init(config=config):
-        for metric_key, metric_values in std_dict.items():
-            wandb.log({f'{metric_key}_std':np.std(metric_values)})
 
 def create_sweep_config(parameters_dict:dict,method:str='grid') -> dict:
     """ Create the sweep config for the wandb grid search.
@@ -232,6 +258,13 @@ def create_sweep_config(parameters_dict:dict,method:str='grid') -> dict:
     sweep_config['metric'] = metric
     sweep_config['parameters'] = parameters_dict
     return sweep_config
+
+def std_logging(config:Config=None,std_dict:dict=dict()):
+    """ Register the std for all metrics. """
+    # Log standard deviations
+    with wandb.init(config=config):
+        for metric_key, metric_values in std_dict.items():
+            wandb.log({f'{metric_key}_std':np.std(metric_values)})
 
 def training_pipeline(parameters_dict:dict,project_name:str,method:str='grid'):
     """ Hyper parameter search with cross-validation for a GNN. """
@@ -262,13 +295,13 @@ def training_single_fold(config:Config=None,fold_id:int=0,std_dict:dict=dict()):
         trainer = Trainer(config,fold_id)
         trainer.train()
         # Assign target specific variables
-        metric_list = trainer.logger.metric_list
-        cross_val_dict = trainer.logger.cross_val_dict
+        metric_list = trainer.logger.get_metric_list()
+        fold_metric_dict = trainer.logger.fold_metric_dict
         # Log global metrics
         global_metric_dict = dict()
-        for metric_name, metric_values in cross_val_dict.items():
+        for metric_name, metric_values in fold_metric_dict.items():
             if metric_name in metric_list:
-                if config.categorical:
+                if config.task_type == 'categorical':
                     if 'loss' in metric_name:
                         metric_key = f'min_{metric_name}'
                         global_metric_dict[metric_key] = np.min(metric_values)
